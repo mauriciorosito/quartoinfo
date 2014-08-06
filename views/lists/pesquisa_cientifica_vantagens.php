88e6df10f3e419c8a152b846522dfdf457b92620
mauricio.rosito <mauricio.rosito@192.168.3.140>
2014-08-06 08:59:31 -0500

    mudanças

---
+++ views/lists/pesquisa_cientifica_vantagens.php
@@ -0,0 +1,50 @@
+﻿<?php
+include_once("../parts/header.php");
+?>
+<meta charset="utf-8">
+					
+		<!--	<div style="margin-left: 11.5%">-->
+                <div id="body" class="col-lg-12 col-md-12 col-sm-12 col-xs-15 pull-right" >
+                    <div class="row">
+                        <div id="menuL" class="col-sm-3 col-md-2 sidebar" role="navigation">
+							<ul class="nav nav-sidebar">
+								<li><a href="pesquisa_apresentacao.php">Apresentação</a></li>
+								<li><a href="pesquisa_comissoes.php">Comissões</a></li>
+								<li><a href="pesquisa_grupos.php">Grupos de Pesquisa</a></li>
+								<li><a href="pesquisa_docencia.php">Iniciação à Docência</a></li>
+								<li class="active"><a href="pesquisa_cientifica.php">Iniciação Científica</a></li>
+								
+								<ul class="escondido"> 
+									
+										<li><a href="pesquisa_cientifica_vantagens.php">Vantagens</a></li>
+										<li><a href="pesquisa_cientifica_como.php">Como se Candidatar?</a></li>										
+									</ul>
+								
+								<li><a href="pesquisa_projetos.php">Projetos de Pesquisa</a></li>
+								<li><a href="pesquisa_nit.php">NIT</a></li>
+								
+								<li><a href="pesquisa_links.php">Links Úteis</a></li>
+							</ul>
+						</div>
+						<div id="conteudo" class="well">
+						<h3><b>Vantagens</b></h3> <br>						
+							<p style="text-align: justify">O contato direto com pesquisas acadêmicas, amplia os conhecimentos adquiridos pelo aluno
+							durante seu curso, além de prepará-lo para a continuação de seus estudos. </p>
+						</div>
+						
+          
+                  <?php
+							include_once("../parts/footer.php");
+				?>
+            </div>
+		</div>
+            <!-- /.container -->
+        </div>
+        <!-- /#content -->
+
+
+       
+
+    </body>
+
+
