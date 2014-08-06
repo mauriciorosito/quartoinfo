765a7f4fc59f817a0dd10a69d977e1c48008386d
mauriciorosito <mauricio.rosito@bento.ifrs.edu.br>
2014-08-06 10:54:20 -0300

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        novo online
+        novo online mesmo
         <?php
         // put your code here
         ?>
