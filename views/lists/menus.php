8a076e3ee2705a0fd3352adda8964c9511f71ce6
giovani.230296 <giovani.230296@192.168.3.163>
2014-08-20 07:48:02 -0500

    Nova página de edição de menus e pequenas alterações no css relacionadas a ela

---
+++ views/lists/menus.php
@@ -0,0 +1,51 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<!-- Login http://www.html5dev.com.br/category/bootstrap/-->
+<html lang="en">
+
+    <head>
+	
+        <meta charset="utf-8">
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link href="../../publics/css/style.css" rel="stylesheet">
+        <link href="../../publics/css/bootstrap.css" rel="stylesheet">
+        <script src="../../packages/wysiwyg/lib/js/bootstrap.min.js"></script>            
+        <noscript src="../../publics/js/jasny-bootstrap.min.js"></noscript>
+    </head>
+    <body>
+        <div id="contentEditMenu">
+            <h1>Lista de Menus</h1>
+            <table bgcolor="white" class="table table-hover">
+                <tr align="center">
+                    <td>Posição</td>
+                    <td>Nome</td>
+                    <td>Localização</td>
+                    <td>Descrição</td>
+                    <td>Gerenciar</td>
+                </tr>
+                <tr align="center">
+                        <td>1.2</td>
+                        <td>Item Menu</td>
+                        <td>Esquerda</td>
+                        <td>Tristeza esse menu, pqp.</td>
+                        <td>
+                            <input class="btn btn-default" type="button" name="editMenu" value="Editar">
+                            <input class="btn btn-default" type="button" name="removeMenu" value="Remover">
+                        </td>
+                </tr>
+                <tr align="center">
+                        <td>3.4</td>
+                        <td>Item Menu</td>
+                        <td>Superior</td>
+                        <td>Tristeza esse menu, pqp.²</td>
+                        <td>
+                            <input class="btn btn-default" type="button" name="editMenu" value="Editar">
+                            <input class="btn btn-default" type="button" name="removeMenu" value="Remover">
+                        </td>
+                </tr>
+            </table>
+        </div>
+    </body>
+</html>
