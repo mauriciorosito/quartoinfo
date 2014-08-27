4eb88ec32f7ff66426414d05acad20d95be9c8f2
fhb12345 <fernando.bandeira@outlook.com>
2014-08-27 09:51:07 -0300

    Update home.list.php
    alterado form pesquisar

--- views/lists/home.list.php
+++ views/lists/home.list.php
@@ -104,14 +104,20 @@ $controllerMedia = new ControllerMedia();
 								<li><a href="../../views/lists/fale_apresentacao.php"><b>Fale conosco</b></a></li>
 							  </ul>
 							  <form class="navbar-form navbar-right" role="search" action="advanced_search.php" method="post">
-                                <div class="form-group">
-                                  								<label for="pesquisar"> 
-								  <input name="pesquisa" type="text" id="pesquisar" class="form-control col-lg-3 col-md-3 col-sm-3 col-xs-3" placeholder="Pesquisar">
-								</label> 
-                                </div>
-                                <button type="submit" class="btn btn-default" style="margin-top: -11px;" name="submit"><span class="glyphicon glyphicon-search"></span></button>
-								<a href="advanced_search.php"><i>Pesquisa Avançada</i></a>
-							  </form>
+                        				<div class="form-group" style="margin-left:-15%;">
+                                                                                        <label for="pesquisar">
+                                                                                           <div class="input-group">
+                                                                                             <input name="pesquisa" type="text" id="pesquisar"  class="form-control col-lg-1 col-md-1 col-sm-1 col-xs-1" placeholder="Pesquisar">
+                                                                                              <span class="input-group-btn">
+                                                                                                        <button type="submit" class="btn btn-default" name="submit">
+                                                                                                        <span class="glyphicon glyphicon-search"></span></button>
+                                                                                                </span>
+                                                                                            </div>
+                                                                                        </label>
+                                                                                        <br>
+                                                                                            <a href="advanced_search.php"><i>Pesquisa Avançada</i></a>
+                                                                                    </div>
+                        				</form>
 							</div>
 						  </div>
 						</nav>
