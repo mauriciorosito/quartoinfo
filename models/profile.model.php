f6ee9d0e15977213ca21c8e607fba55c5e565dbe
maximiliano.300395 <maximiliano.300395@192.168.3.166>
2014-08-27 06:44:45 -0500

    alterações profile e user (models e controls)

--- models/profile.model.php
+++ models/profile.model.php
@@ -1,58 +1,69 @@
 <?php
-	class Profile {
-		private $idProfile;
-		private $type;
-		private $name;
-		private $photo;
-		private $registration;
-		private $about;
 
-		public function setIdProfile($idProfile){
-			$this->idProfile = $idProfile;
-		}
+class Profile {
 
-		public function getIdProfile(){
-			return $this->idProfile;
-		}
+    private $idProfile;
+    private $name;
+    private $is_admin;
+    private $can_edit;
+    private $can_view;
+    private $can_create;
+    private $can_delete;
+    
+    public function getIdProfile() {
+        return $this->idProfile;
+    }
 
-		public function setType($type){
-			$this->type = $type;
-		}
+    public function getName() {
+        return $this->name;
+    }
 
-		public function getType(){
-			return $this->type;
-		}
+    public function getIs_admin() {
+        return $this->is_admin;
+    }
 
-		public function setName($name){
-			$this->name = $name;
-		}
+    public function getCan_edit() {
+        return $this->can_edit;
+    }
 
-		public function getName(){
-			return $this->name;
-		}
+    public function getCan_view() {
+        return $this->can_view;
+    }
 
-		public function setPhoto($photo){
-			$this->photo = $photo;
-		}
+    public function getCan_create() {
+        return $this->can_create;
+    }
 
-		public function getPhoto(){
-			return $this->photo;
-		}
+    public function getCan_delete() {
+        return $this->can_delete;
+    }
 
-		public function setRegistration($registration){
-			$this->registration = $registration;
-		}
+    public function setIdProfile($idProfile) {
+        $this->idProfile = $idProfile;
+    }
 
-		public function getRegistration(){
-			return $this->registration;
-		}
+    public function setName($name) {
+        $this->name = $name;
+    }
 
-		public function setAbout($about){
-			$this->about = $about;
-		}
+    public function setIs_admin($is_admin) {
+        $this->is_admin = $is_admin;
+    }
 
-		public function getAbout(){
-			return $this->about;
-		}
-	}
-?>
+    public function setCan_edit($can_edit) {
+        $this->can_edit = $can_edit;
+    }
+
+    public function setCan_view($can_view) {
+        $this->can_view = $can_view;
+    }
+
+    public function setCan_create($can_create) {
+        $this->can_create = $can_create;
+    }
+
+    public function setCan_delete($can_delete) {
+        $this->can_delete = $can_delete;
+    }
+    
+}
--- models/user.model.php
+++ models/user.model.php
@@ -12,89 +12,121 @@ class User {
     private $reminderResponse;
     private $canReceiveContent;
     private $type;
-   
-
+    private $name;
+    private $photo;
+    private $registration;
+    private $about;
     
-    public function getType() {
-        return $this->type;
-    }
-
-    public function setType($type) {
-        $this->type = $type;
-    }
-
-    public function setIdUser($idUser) {
-        $this->idUser = $idUser;
-    }
-
     public function getIdUser() {
         return $this->idUser;
-    }
-
-    public function setIdProfile($idProfile) {
-        $this->idProfile = $idProfile;
     }
 
     public function getIdProfile() {
         return $this->idProfile;
     }
 
-    public function setIdCourse($idCourse) {
-        $this->idCourse = $idCourse;
-    }
-
     public function getIdCourse() {
         return $this->idCourse;
-    }
-
-    public function setEmail($email) {
-        $this->email = $email;
     }
 
     public function getEmail() {
         return $this->email;
     }
 
-    public function setLogin($login) {
-        $this->login = $login;
-    }
-
     public function getLogin() {
         return $this->login;
-    }
-
-    public function setHash($hash) {
-        $this->hash = $hash;
     }
 
     public function getHash() {
         return $this->hash;
     }
 
-    public function setReminder($reminder) {
-        $this->reminder = $reminder;
-    }
-
     public function getReminder() {
         return $this->reminder;
-    }
-
-    public function setReminderResponse($reminderResponse) {
-        $this->reminderResponse = $reminderResponse;
     }
 
     public function getReminderResponse() {
         return $this->reminderResponse;
     }
 
-    public function setCanReceiveContent($canReceiveContent) {
-        $this->canReceiveContent = $canReceiveContent;
-    }
-
     public function getCanReceiveContent() {
         return $this->canReceiveContent;
     }
 
-}
+    public function getType() {
+        return $this->type;
+    }
 
-?>
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getPhoto() {
+        return $this->photo;
+    }
+
+    public function getRegistration() {
+        return $this->registration;
+    }
+
+    public function getAbout() {
+        return $this->about;
+    }
+
+    public function setIdUser($idUser) {
+        $this->idUser = $idUser;
+    }
+
+    public function setIdProfile($idProfile) {
+        $this->idProfile = $idProfile;
+    }
+
+    public function setIdCourse($idCourse) {
+        $this->idCourse = $idCourse;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function setLogin($login) {
+        $this->login = $login;
+    }
+
+    public function setHash($hash) {
+        $this->hash = $hash;
+    }
+
+    public function setReminder($reminder) {
+        $this->reminder = $reminder;
+    }
+
+    public function setReminderResponse($reminderResponse) {
+        $this->reminderResponse = $reminderResponse;
+    }
+
+    public function setCanReceiveContent($canReceiveContent) {
+        $this->canReceiveContent = $canReceiveContent;
+    }
+
+    public function setType($type) {
+        $this->type = $type;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function setPhoto($photo) {
+        $this->photo = $photo;
+    }
+
+    public function setRegistration($registration) {
+        $this->registration = $registration;
+    }
+
+    public function setAbout($about) {
+        $this->about = $about;
+    }
+    
+}
