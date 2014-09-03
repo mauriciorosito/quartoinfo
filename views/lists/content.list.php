6da52ca6b9958ed7c4fdd5ad6d28edb7d17397d3
fhb12345 <fernando.bandeira@outlook.com>
2014-09-03 08:52:14 -0300

    pesquisa feita

--- views/lists/content.list.php
+++ views/lists/content.list.php
@@ -1,7 +1,8 @@
 ﻿<?php
 include_once("../../controllers/content.control.php");
+require_once("../../packages/system/functions.model.php");
 $controllerContent = new ControllerContent();
-$contents = $controllerContent->actionControl('selectAllEvents');
+if(!isset($_POST['pesquisa'])) $contents = $controllerContent->actionControl('selectAllEvents');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -40,7 +41,32 @@ $contents = $controllerContent->actionControl('selectAllEvents');
         <div id="content">
             <div class="container img-rounded BVerde">
                 <a href="../forms/content.form.php?action=insert" class="btn btn-default">Inserir</a> 
-                <table class="table table-striped table-condensed table-bordered table-hover">
+                <form class="navbar-form navbar-right" role="search" action="content.list.php" method="post">
+                    <div class="form-group" style="margin-left:-15%;">
+                        <label for="pesquisar">
+                           <div class="input-group">
+                             <input name="pesquisa" type="text" id="pesquisar"  class="form-control col-lg-1 col-md-1 col-sm-1 col-xs-1" placeholder="Pesquisar">
+                              <span class="input-group-btn">
+                                        <button type="submit" class="btn btn-default" name="submit">
+                                        <span class="glyphicon glyphicon-search"></span></button>
+                                </span>
+                            </div>
+                        </label>
+                    </div>
+                </form>
+                <?php
+                        if(isset($_POST['page'])) $page = $_POST['page'];
+                        else $page = 1;
+                        if(isset($_POST["submit"])) {
+                                if(isset($_POST["pesquisa"])) {
+                                    echo '<h1> Pesquisando por:'.$_POST['pesquisa'].'</h1>';
+                                        $pesquisa = new functions();
+
+                                        $resultados = ($pesquisa->searchAll($_POST["pesquisa"], $page));
+                                }
+                        }
+                ?>	
+                <table class="table table-striped table-condensed table-bordered table-hover" id="tabela">
                     <thead>
                         <tr>
                             <th>#</th>
@@ -59,6 +85,7 @@ $contents = $controllerContent->actionControl('selectAllEvents');
                     </thead>
                     <tbody data-link="row" class="rowlink">
                         <?php
+                        if(!isset($_POST['pesquisa'])) {
                         foreach ($contents as $content) {
                             ?>
                             <tr>
@@ -86,11 +113,32 @@ $contents = $controllerContent->actionControl('selectAllEvents');
                                     ?></td>
                                 <td><a href="../forms/content.form.php?action=update&idContent=<?php echo $content->getIdContent(); ?> " class="btn btn-default">Editar</a> <a href="../forms/content.form.php?action=delete&idContent=<?php echo $content->getIdContent(); ?>" class="btn btn-default">Excluir</a></td>	
                                 <?php
-                            }
-                            ?>					
+                            }} else {
+                                if(isset($resultados)){
+								foreach($resultados as $resultado){
+						?>
+						<tr>
+							<td><?php echo $resultado["publisher"]; ?></td>
+							<td><?php echo $resultado["source"]; ?></td>
+							<td><?php echo $resultado["title"]; ?></td>
+							<td><?php echo $resultado["text"]; ?></td>
+                                                        <td><?php echo $resultado["description"]; ?></td>
+                                                        <td><?php echo $resultado["postDate"]; ?></td>
+                                                        <td><?php echo $resultado["expirationDate"]; ?></td>
+                                                        <td><?php echo $resultado["type"]; ?></td>
+						</tr>
+						<?php
+								}
+                            }}
+						?>		
                         </tr>
                     </tbody>
                 </table>
+                <?php
+                        if(isset($pesquisa)){
+                                $pesquisa->pagination($pesquisa->total, $page);
+                        }
+                ?>
             </div>
             <!-- /.container -->
         </div>
@@ -98,13 +146,29 @@ $contents = $controllerContent->actionControl('selectAllEvents');
 
 
         <!-- JavaScript -->
-        <script src="publics/js/jquery-1.10.2.js"></script>
-        <script src="publics/js/bootstrap.js"></script>
-        <script src="publics/js/craftyslide.js"></script>
-        <script src="publics/js/script.js"></script>
-        <script type="text/javascript" src="publics/js/rhinoslider-1.05.js"></script>
-        <script type="text/javascript" src="publics/js/mousewheel.js"></script>
-        <script type="text/javascript" src="publics/js/easing.js"></script>
+        <script src="../../publics/js/jquery-1.10.2.js"></script>
+        <script src="../../publics/js/bootstrap.js"></script>
+        <script src="../../publics/js/craftyslide.js"></script>
+        <script src="../../publics/js/script.js"></script>
+        <script type="text/javascript" src="../../publics/js/rhinoslider-1.05.js"></script>
+        <script type="text/javascript" src="../../publics/js/mousewheel.js"></script>
+        <script type="text/javascript" src="../../publics/js/easing.js"></script>
+        
+        <script>
+            $( ".page" ).click(function() {
+                page = $( this ).html();
+                $(".page").parent().removeClass("active");
+                $(this).parent().addClass("active");
+                $.ajax({
+                    type: 'post',
+                    data: 'page='+page+'&pesquisa=<?php echo $_POST['pesquisa']; ?>&submit=<?php echo $_POST['submit']; ?>',
+                    url:'../parts/content.php',
+                    success: function(retorno){
+                      $('#tabela').html(retorno);  
+                    }
+                })
+            });
+        </script>
 
     </body>
 
