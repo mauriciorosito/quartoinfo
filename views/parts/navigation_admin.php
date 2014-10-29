fc4a224f8777798132a7034055e0fc13146804aa
fhb12345 <fernando.bandeira@outlook.com>
2014-10-29 08:15:58 -0300

    nova pagina de lsitagem de paginas

--- views/parts/navigation_admin.php
+++ views/parts/navigation_admin.php
@@ -17,7 +17,7 @@
                 <li><a href="course.list.php"><i class="glyphicon glyphicon-book"></i> &nbsp; Cursos</a></li>
                 <li><a href="profile.list.php"><i class="glyphicon glyphicon-tree-deciduous"></i> &nbsp; Perfil</a></li>
                 <li><a href="secao.list.php"><i class="glyphicon glyphicon-list-alt"></i> &nbsp; Seção</a></li>
-               
+                <li><a href="pages.list.php"><i class="glyphicon glyphicon-list-alt"></i> &nbsp; Páginas</a></li>
             </ul>
 
             <ul class="nav navbar-nav navbar-right">
